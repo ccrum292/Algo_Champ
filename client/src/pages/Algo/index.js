71c948bc619e8ec41b9984c414224d6de424409c
ccrum292 <ccrum292@gmail.com>
2021-03-14 11:51:43 -0400

    styled codeMirror

--- client/src/pages/Algo/index.js
+++ client/src/pages/Algo/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import clsx from 'clsx';
-
+import "./style.css";
 
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
+import {UnControlled as CodeMirror} from 'react-codemirror2';
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/material.css');
+require('codemirror/mode/javascript/javascript.js');
 
-const drawerWidth = 240;
+
 
 const useStyles = makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
@@ -41,25 +45,49 @@ const useStyles = makeStyles((theme) => ({
   },
   secondSmallPaper: {
     marginTop: theme.spacing(2),
+  },
+  fullCodeMirror: {
+    height: "100%"
   }
 }));
 
 export default function Algo(props) {
   const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  const fixedHeightPaper2 = clsx(classes.paper, classes.fixedHeightConsole);
+  const tableDivPaper = clsx(classes.paper, classes.tableDiv, classes.secondSmallPaper);
 
   let params = useParams();
   console.log("params",params);
 
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-  const fixedHeightPaper2 = clsx(classes.paper, classes.fixedHeightConsole);
-  const tableDivPaper = clsx(classes.paper, classes.tableDiv, classes.secondSmallPaper);
+  let options = {
+    mode: 'javascript',
+    theme: 'material',
+    lineNumbers: true,
+  }
+
+  let value = '// Hello World';
+
+  const [codeMirrorValue, setCodeMirrorValue] = useState(value);
+
+  console.log(codeMirrorValue);
+
+  
+  
   return (
         <Container component="main" maxWidth="lg" className={classes.container}>
           <div className={classes.appBarSpacer}></div>
           <Grid container spacing={3} className={classes.outerGrid}>
             <Grid container justify="space-between"  direction="column" item xs={12} md={7} lg={8}>
               <Paper className={fixedHeightPaper2}>
-                Algo
+                <CodeMirror
+                  className={classes.fullCodeMirror}
+                  value={value}
+                  options={options}
+                  onChange={(editor, data, value) => {
+                    setCodeMirrorValue(value);
+                  }}
+                />
               </Paper>
               <Paper className={tableDivPaper}>
                 Console 
