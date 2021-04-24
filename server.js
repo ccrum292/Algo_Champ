0a13fcc2492b91353c47ff82d603907c240c66f0
ccrum292 <ccrum292@gmail.com>
2021-04-24 11:42:41 -0400

    updating server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(passport.initialize());
 
 //-- Static Server (Production) ----------------------------------------------
 if (process.env.NODE_ENV === "production") {
-  const clientBuildPath = path.join(__dirname, "..", "client", "build");
+  const clientBuildPath = path.join(__dirname, "client", "build");
   console.log(`Client build path: ${clientBuildPath}\n`);
   app.use(express.static(clientBuildPath));
 }
