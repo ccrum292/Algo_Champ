ef16c81ca38455bb576684610bd296ca351bf77b
ccrum292 <ccrum292@gmail.com>
2021-04-24 13:20:22 -0400

    updated readme

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(require("./controllers"));
 
 //-- React catch-all ---------------------------------------------------------
 app.get("*", (req, res) => {
-  console.log(path.join(__dirname, "./client/build/index.html"), "./client/build/index.html")
+  // console.log(path.join(__dirname, "./client/build/index.html"), "./client/build/index.html")
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
